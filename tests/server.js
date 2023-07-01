86d6583eb8ad7aa4ab439bcc26f21382768d5790
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-07-01 23:05:06 +0200

    feat: update mock server with new data schema

--- tests/server.js
+++ tests/server.js
@@ -1,94 +1,89 @@
 import Http from "http";
 
-const EXAMPLE = [
-  {
-    gameId: 1,
-    gameName: "Minecraft",
-    platform: "PC",
-    submitter: "fajnyCreeper",
-    category: "GOTY",
-    votes: 10,
-  },
-  {
-    gameId: 2,
-    gameName: "PUBG",
-    platform: "Playstation",
-    submitter: "fillipp_",
-    category: "GOTY",
-    votes: 8,
-  },
-  {
-    gameId: 3,
-    gameName: "Csko",
-    platform: "Xbox",
-    submitter: "KoriTRB",
-    category: "GOTY",
-    votes: 16,
-  },
-  {
-    gameId: 4,
-    gameName: "Raketa",
-    platform: "PC",
-    submitter: "Zwejra",
-    category: "GOTY",
-    votes: 7,
-  },
-  {
-    gameId: 5,
-    gameName: "Dynamit",
-    platform: "PC",
-    submitter: "BaBca_",
-    category: "GOTY",
-    votes: 25,
-  },
-];
+const PUMP_MODIFIER = 1.1;
+const DELETION_MODIFIER = 1;
 
-const EXAMPLE_TOTAL = () =>
-  EXAMPLE.reduce((acc, e) => acc + e.votes, 0) +
-  Math.floor(Math.random() * 1337);
+const EXAMPLE = {
+  total_votes: 760,
+  entries: [
+    {
+      entry: {
+        id: 9,
+        name: "Elden Ring",
+        category: {
+          id: 1,
+          name: "Nejlepší singleplayer hra roku",
+          channel: "1053793977586946140",
+        },
+        user: {
+          id: 19,
+          snowflake: "111111111111111111",
+          user_name: "FrankFrank",
+        },
+        state: 1,
+        message: null,
+      },
+      votes: 258,
+    },
+    {
+      entry: {
+        id: 7,
+        name: "God of War Ragnarök",
+        category: {
+          id: 1,
+          name: "Nejlepší singleplayer hra roku",
+          channel: "1053793977586946140",
+        },
+        user: {
+          id: 17,
+          snowflake: "222222222222222222",
+          user_name: "TomTomTom",
+        },
+        state: 1,
+        message: null,
+      },
+      votes: 208,
+    },
+  ],
+};
 
 const PUMP_VOTES = (count) => {
   for (let i = 0; i < count; i++) {
-    const id = Math.floor(Math.random() * EXAMPLE.length);
-    EXAMPLE[id].votes = Math.floor(EXAMPLE[id].votes * 1.2);
+    const id = Math.floor(Math.random() * EXAMPLE.entries.length);
+    EXAMPLE.entries[id].votes = Math.floor(
+      EXAMPLE.entries[id].votes * PUMP_MODIFIER
+    );
   }
 
   let total = 0;
-  EXAMPLE.forEach((e) => (total += e.votes));
+  EXAMPLE.entries.forEach((e) => (total += e.votes));
   if (total > 100000000) {
-    EXAMPLE.forEach((e) => (e.votes = 10));
+    EXAMPLE.entries.forEach((e) => (e.votes = 10));
   }
 };
 
 const CLEAR_EXAMPLE = () => {
-  return EXAMPLE.filter(() => Math.random() < 0.8);
+  return EXAMPLE.entries.filter(() => Math.random() < DELETION_MODIFIER);
 };
 
 setInterval(() => PUMP_VOTES(3), 100);
 
 const requestListener = function (req, res) {
-  if (req.url.startsWith("/list")) {
-    res.writeHead(200);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+  if (req.url.startsWith("/data")) {
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    });
     res.write(JSON.stringify(CLEAR_EXAMPLE()));
-    res.end();
-  } else if (req.url.startsWith("/total")) {
-    res.writeHead(200);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-    res.write(
-      JSON.stringify({
-        category: null,
-        totalVotes: EXAMPLE_TOTAL(),
-      })
-    );
     res.end();
   } else {
     res.writeHead(404);
+    res.write("not found");
     res.end();
   }
 };
 
 const server = Http.createServer(requestListener);
 server.listen(8080);
+
+console.log("Mock server started on port 8080");
