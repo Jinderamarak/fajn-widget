c5ce735981d5040bcc21e67302a76c688f65f75c
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-07-08 19:44:45 +0200

    feat: new mock server

--- tests/server.js
+++ tests/server.js
@@ -1,86 +1,193 @@
 import Http from "http";
 
-const PUMP_MODIFIER = 1.1;
-const DELETION_MODIFIER = 1;
+const gameNames = [
+  "Elden Ring",
+  "God of War Ragnarök",
+  "Halo Infinite",
+  "Horizon Forbidden West",
+  "Starfield",
+  "The Legend of Zelda: Breath of the Wild 2",
+  "The Elder Scrolls VI",
+  "Final Fantasy XVI",
+  "Gran Turismo 7",
+  "Forza Horizon 5",
+  "Gotham Knights",
+  "Minecraft",
+  "Deathloop",
+  "Far Cry 6",
+  "Ratchet & Clank: Rift Apart",
+  "Kena: Bridge of Spirits",
+  "Psychonauts 2",
+  "Resident Evil Village",
+  "GhostWire: Tokyo",
+  "Back 4 Blood",
+  "Dying Light 2",
+  "Rainbow Six Quarantine",
+  "Vampire: The Masquerade - Bloodlines 2",
+  "The Lord of the Rings: Gollum",
+  "The Callisto Protocol",
+  "Atomic Heart",
+  "The Medium",
+  "Returnal",
+  "Chivalry 2",
+  "S.T.A.L.K.E.R. 2",
+  "The Ascent",
+  "CrossfireX",
+  "Scorn",
+  "The Outlast Trials",
+  "The Stanley Parable: Ultra Deluxe",
+  "Tunic",
+  "Twelve Minutes",
+  "The Gunk",
+  "The Good Life",
+  "The Artful Escape",
+  "The Big Con",
+  "The Forgotten City",
+  "The Last Night",
+  "The Wolf Among Us 2",
+  "Trek to Yomi",
+  "Tunic",
+  "Twelve Minutes",
+  "Unexplored 2: The Wayfarer's Legacy",
+  "Warhammer 40,000: Darktide",
+  "Way to the Woods",
+  "The Witch Queen",
+  "Senua's Saga: Hellblade II",
+  "Fable",
+  "Everwild",
+  "Avowed",
+];
 
-const EXAMPLE = {
-  total_votes: 760,
-  entries: [
-    {
-      entry: {
-        id: 9,
-        name: "Elden Ring",
-        category: {
-          id: 1,
-          name: "Nejlepší singleplayer hra roku",
-          channel: "1053793977586946140",
-        },
-        user: {
-          id: 19,
-          snowflake: "111111111111111111",
-          user_name: "FrankFrank",
-        },
-        state: 1,
-        message: null,
-      },
-      votes: 258,
-    },
-    {
-      entry: {
-        id: 7,
-        name: "God of War Ragnarök",
-        category: {
-          id: 1,
-          name: "Nejlepší singleplayer hra roku",
-          channel: "1053793977586946140",
-        },
-        user: {
-          id: 17,
-          snowflake: "222222222222222222",
-          user_name: "TomTomTom",
-        },
-        state: 1,
-        message: null,
-      },
-      votes: 208,
-    },
-  ],
+const userNames = [
+  "Tom",
+  "Frank",
+  "John",
+  "Peter",
+  "Michael",
+  "Robert",
+  "William",
+  "David",
+  "Richard",
+  "Joseph",
+  "Thomas",
+  "Charles",
+  "Christopher",
+  "Daniel",
+  "Matthew",
+  "Anthony",
+  "Donald",
+  "Mark",
+  "Paul",
+  "Steven",
+  "Andrew",
+  "Kenneth",
+  "Joshua",
+  "George",
+  "Kevin",
+  "Brian",
+  "Edward",
+  "Ronald",
+  "Timothy",
+  "Jason",
+  "Jeffrey",
+  "Ryan",
+  "Jacob",
+  "Gary",
+  "Nicholas",
+  "Eric",
+  "Stephen",
+];
+
+const categoryNames = [
+  "Best Singleplayer Game",
+  "Best Game of the Year",
+  "Best Multiplayer Game",
+  "Best Indie Game",
+  "Best Action Game",
+  "Best Adventure Game",
+  "Best RPG",
+  "Best Shooter",
+  "Best Fighting Game",
+  "Best Racing Game",
+  "Best Sports Game",
+  "Best Strategy Game",
+  "Best Family Game",
+  "Best VR Game",
+  "Best Mobile Game",
+  "Best Ongoing Game",
+  "Best Game Direction",
+  "Best Narrative",
+  "Best Art Direction",
+  "Best Score and Music",
+  "Best Audio Design",
+  "Best Performance",
+  "Games for Impact",
+  "Best Community Support",
+  "Best Debut Game",
+];
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const PUMP_VOTES = (count) => {
+const createUser = () => {
+  const name = userNames[randomInt(0, userNames.length - 1)];
+  return {
+    id: randomInt(0, 100000),
+    snowflake: randomInt(0, 1000000000000000000),
+    user_name: name,
+  };
+};
+
+const createCategory = () => {
+  const name = categoryNames[randomInt(0, categoryNames.length - 1)];
+  return {
+    id: randomInt(0, 100000),
+    name: name,
+    channel: randomInt(0, 1000000000000000000),
+  };
+};
+
+const createEntry = (id, name) => {
+  return {
+    votes: 0,
+    entry: {
+      id: id,
+      name: name,
+      category: createCategory(),
+      user: createUser(),
+      state: 1,
+      message: null,
+    },
+  };
+};
+
+const createInitialData = () => {
+  const entries = gameNames.map((name, index) => createEntry(index, name));
+  return {
+    total_votes: 0,
+    entries: entries,
+  };
+};
+const addVotes = (data, count) => {
   for (let i = 0; i < count; i++) {
-    const id = Math.floor(Math.random() * EXAMPLE.entries.length);
-    EXAMPLE.entries[id].votes = Math.floor(
-      EXAMPLE.entries[id].votes * PUMP_MODIFIER
-    );
-  }
-
-  let total = 0;
-  EXAMPLE.entries.forEach((e) => (total += e.votes));
-  if (total > 100000000) {
-    EXAMPLE.entries.forEach((e) => (e.votes = 10));
+    const id = randomInt(0, data.entries.length);
+    const diff = id + 1;
+    data.entries[id].votes += diff;
+    data.total_votes += diff;
   }
 };
 
-const CLEAR_EXAMPLE = () => {
-  return EXAMPLE.entries.filter(() => Math.random() < DELETION_MODIFIER);
-};
-
-setInterval(() => PUMP_VOTES(3), 100);
+const db = createInitialData();
+setInterval(() => addVotes(db, 1), 1);
 
 const requestListener = function (req, res) {
-  if (req.url.startsWith("/data")) {
-    res.writeHead(200, {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    });
-    res.write(JSON.stringify(CLEAR_EXAMPLE()));
-    res.end();
-  } else {
-    res.writeHead(404);
-    res.write("not found");
-    res.end();
-  }
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  });
+  res.write(JSON.stringify(db));
+  res.end();
 };
 
 const server = Http.createServer(requestListener);
