5bbbaa33b23b9c358fa9542ff78bfec034f1b841
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-08-05 20:54:24 +0200

    build: try relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isDeployment = process.env.IS_GITHUB_ACTION === "yes";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isDeployment ? "/fajn-widget/" : "/",
+  base: "./",
   build: {
     outDir: "build",
   },
